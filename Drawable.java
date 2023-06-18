04dbee0564da3e67ad6117f231decad1891f7365
Danial Moczynski <131328813+danny-moczynski@users.noreply.github.com>
2023-06-18 13:50:48 -0500

    Create Drawable.java
    The Drawable interface serves as a blueprint for objects that can be drawn, but it does not provide any concrete functionality. It allows different classes to define their own way of drawing themselves by implementing the draw method according to their specific requirements.

---
+++ Drawable.java
@@ -0,0 +1,15 @@
+package artist;
+import artist.StdDraw;
+
+public interface Drawable {
+  /*
+   * This method: abstract method for draw in the drawable class
+   * 
+   * @param: none
+   * 
+   * return type: void method
+   */
+  public abstract void draw();
+  
+  
+}

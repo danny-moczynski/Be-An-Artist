a0eec39b64f2c8a9dac5d3cc54c2497979419c39
Danial Moczynski <131328813+danny-moczynski@users.noreply.github.com>
2023-06-18 13:54:06 -0500

    Create Point.java
    The Point class represents a point in a two-dimensional coordinate system and provides a constructor for creating point objects with specified coordinates.

---
+++ Point.java
@@ -0,0 +1,18 @@
+package artist;
+import artist.StdDraw;
+
+public class Point {
+  public double x;
+  public double y;
+  /*
+   * This method: Point, sets where x and y on canvas
+   * 
+   * @param: double x, double y
+   * 
+   * return type: no return type because it is a constructor
+   */
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+}
